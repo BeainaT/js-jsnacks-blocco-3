f0cfe0beffc1cdd2d58b0920ce42a7838d01b5e1
BeainaT <tania.df@hotmail.it>
2022-05-05 15:23:05 +0200

    added array generator by input

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,4 +1,10 @@
 //Chiedi all'utente di inserire un numero N
+const userNumber = Number(prompt("Inserisci un numero"));
+let newArray = [];
 //Genera quindi N array
+for ( let i = 0; i < userNumber; i++ ) {
+    console.log(newArray);
+}
 //Ogni array è formato da 10 numeri casuali tra 1 e 100
 //Stampa a schermo un array ogni volta che viene generato.
+// console.log(newArray)
