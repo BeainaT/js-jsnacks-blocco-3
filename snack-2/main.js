f89ddabdf06fdb3e4e03173524a1a0154a44b796
BeainaT <tania.df@hotmail.it>
2022-05-05 21:01:22 +0200

    fixed - printed all in one

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,18 +1,16 @@
 //Chiedi all'utente di inserire un numero N
 const userNumber = Number(prompt("Inserisci un numero"));
-let newArray = [];
-let randomNumber;
+
 //Genera quindi N array
-for ( let i = 0; i < userNumber; i++ ) {
-    randomNumber = Math.floor(Math.random() * 100) + 1; //Genero numero random tra 1 e 100
-    newArray.push(randomNumber); //aggiungo il numero random all'array
-    console.log(newArray);
-}
-//Ogni array è formato da 10 numeri casuali tra 1 e 100
-const arrayLenght = Math.floor(Math.random() * 10) + 1;
-for ( i = 0; i < arrayLenght; i++ ) {
-    newArray.push(randomNumber);
+for (let i = 0; i < userNumber; i++) {
+    let newArray = [] //includo costante nel ciclo che verrà generata dall'input (userNumber)
+    //Ogni array è formato da 10 numeri casuali tra 1 e 100
+    for ( let i = 0; i < 10; i++) {
+        let randomNumber = Math.floor(Math.random() * 100) + 1; //Genero numero random tra 1 e 100 includendo NEL CICLO la costante
+        newArray.push(randomNumber)//aggiungo il numero random all'array
+    }
+    //stampa degli array generati
+    console.log(newArray)
 }
 
 //Stampa a schermo un array ogni volta che viene generato.
-// console.log(newArray)
