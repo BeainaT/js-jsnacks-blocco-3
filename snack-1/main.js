97d308881e5ef2586da33e0fcf9be05564bd8f8d
BeainaT <tania.df@hotmail.it>
2022-05-05 12:38:18 +0200

    created array + push

---
+++ snack-1/main.js
@@ -0,0 +1,8 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+//Crea un array vuoto
+let arr = [];
+//Chiedere all'utente un numero da inserire nell'array
+let userNumber = Number(prompt("Inserisci un numero"));
+arr.push(userNumber);
+let sum = 0 + userNumber;
+console.log(sum);
