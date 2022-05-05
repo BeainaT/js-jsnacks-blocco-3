f45393019e244d612dc04ac46ccdc4ecf5f7c8dc
BeainaT <tania.df@hotmail.it>
2022-05-05 15:07:52 +0200

    added files

---
+++ snack-2/main.js
@@ -0,0 +1,4 @@
+//Chiedi all'utente di inserire un numero N
+//Genera quindi N array
+//Ogni array è formato da 10 numeri casuali tra 1 e 100
+//Stampa a schermo un array ogni volta che viene generato.
