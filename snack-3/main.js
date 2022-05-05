6b05a08688ff3c2e651dc61725e7ec43ff51430c
BeainaT <tania.df@hotmail.it>
2022-05-05 18:03:37 +0200

    added snack code

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,7 +1,32 @@
 // Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
-const oddNumber = document.createElement("div");
-const body = document.querySelector("body");
-body.append(oddNumber);
+
+const body = document.querySelector("body"); //Seleziono elemento esistente in HTML
+//Creo due div
+const oddBox = document.createElement("div");
+const evenBox = document.createElement("div");
+//aggiungo i nuovi elementi a body
+body.append(oddBox);
+body.append(evenBox);
+//ad ogni div aggiungo id e colore
+oddBox.classList.add("#red");
+oddBox.style.color = "red";
+evenBox.classList.add("#green");
+evenBox.style.color = "green";
+//Genero un array di numeri
+const myArray = [];
+for ( i = 0; i < 10; i++) {
+    numbers = Math.floor(Math.random() * 10) + 1;
+    myArray.push(numbers);
+    // Se i numeri generati sono pari, li aggiungo a div con #green, altrimenti li aggiungo a div con #red;
+    if (numbers % 2 === 0) {
+        evenBox.append(numbers);
+    } else {
+        oddBox.append(numbers);
+    }
+}
+console.log(myArray);
+
+
 
