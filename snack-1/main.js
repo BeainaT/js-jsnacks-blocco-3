c977685fef1d704d5178cc9348778c267426779d
BeainaT <tania.df@hotmail.it>
2022-05-05 14:51:18 +0200

    fixed

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,13 +1,15 @@
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
-//Crea un array vuoto
+//Creo un array vuoto
 let arr = [];
 //dichiaro una variabile per la somma
 let sum = 0;
+//eseguo ciclo 
 do {
     let userNumber = Number(prompt("Inserisci un numero")); //chiedo all'utente un numero
-    if (userNumber < 50) { //SE il numero aggiunto è < 50
+    sum += userNumber; //aggiorno la somma con il numero inserito dall'utente
+    if (sum < 50) { //SE la somma è < 50
         arr.push(userNumber); //aggiungo il numero inserito dall'utente nell'array
-        sum += userNumber; //aggiorno la somma con il numero inserito dall'utente finchè somma < 50
     }
-} while (sum < 50);
-console.log(sum, arr)
+} while (sum < 50); //esco dal ciclo quando la somma è < 50
+
+console.log(arr);
